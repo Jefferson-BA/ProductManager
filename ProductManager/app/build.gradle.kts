5346c0f0a0c234b94fdf56cb323349200f35fe0b
Jefferson <jefferson.ig.ig@gmail.com>
2025-12-05 15:57:45 -0500

    implementacion del dependencias y creacion de AuthRepository.kt

--- ProductManager/app/build.gradle.kts
+++ ProductManager/app/build.gradle.kts
@@ -59,4 +59,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+
 }
--- ProductManager/app/src/main/java/com/tecsup/productmanager/auth/AuthRepository.kt
+++ ProductManager/app/src/main/java/com/tecsup/productmanager/auth/AuthRepository.kt
@@ -1,4 +1,32 @@
 package com.tecsup.productmanager.auth
 
-class AuthRepository {
+
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.tasks.await
+
+class AuthRepository(
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+) {
+
+    suspend fun login(email: String, password: String): Result<Unit> {
+        return try {
+            auth.signInWithEmailAndPassword(email, password).await()
+            Result.success(Unit)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun register(email: String, password: String): Result<Unit> {
+        return try {
+            auth.createUserWithEmailAndPassword(email, password).await()
+            Result.success(Unit)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    fun logout() = auth.signOut()
+
+    fun currentUserId(): String? = auth.currentUser?.uid
 }
