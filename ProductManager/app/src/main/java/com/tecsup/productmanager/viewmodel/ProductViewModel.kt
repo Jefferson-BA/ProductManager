f6916b6731bff2924268c14c38e7a552ca97404a
Jefferson <jefferson.ig.ig@gmail.com>
2025-12-05 16:02:45 -0500

    creacion del ProductViewModel.kt

--- ProductManager/app/src/main/java/com/tecsup/productmanager/viewmodel/ProductViewModel.kt
+++ ProductManager/app/src/main/java/com/tecsup/productmanager/viewmodel/ProductViewModel.kt
@@ -1,4 +1,31 @@
 package com.tecsup.productmanager.viewmodel
 
-class ProductViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tecsup.productmanager.data.model.Product
+import com.tecsup.productmanager.data.repository.ProductRepository
+import kotlinx.coroutines.launch
+
+class ProductViewModel(
+    private val repo: ProductRepository = ProductRepository()
+) : ViewModel() {
+
+    val products = repo.products
+
+    fun loadProducts(userId: String) {
+        repo.listenProducts(userId)
+    }
+
+    fun saveProduct(product: Product) {
+        viewModelScope.launch {
+            if (product.id.isEmpty()) repo.addProduct(product)
+            else repo.updateProduct(product)
+        }
+    }
+
+    fun deleteProduct(id: String) {
+        viewModelScope.launch {
+            repo.deleteProduct(id)
+        }
+    }
 }
