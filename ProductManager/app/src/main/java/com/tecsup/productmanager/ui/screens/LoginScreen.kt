b3647b4ee96b70d9e98f23693928059ab4686f92
Jefferson <jefferson.ig.ig@gmail.com>
2025-12-05 16:04:45 -0500

    creacion del LoginScreen.kt.kt

--- ProductManager/app/src/main/java/com/tecsup/productmanager/ui/screens/LoginScreen.kt
+++ ProductManager/app/src/main/java/com/tecsup/productmanager/ui/screens/LoginScreen.kt
@@ -1,4 +1,19 @@
 package com.tecsup.productmanager.ui.screens
 
-class LoginScreen {
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.tecsup.productmanager.viewmodel.AuthViewModel
+
+@Composable
+fun LoginScreen(
+    viewModel: AuthViewModel,
+    onSuccess: () -> Unit,
+    onRegisterClick: () -> Unit
+) {
+    val state by viewModel.state.collectAsState()
+
+    if (state.success) {
+        onSuccess()
+    }
 }
