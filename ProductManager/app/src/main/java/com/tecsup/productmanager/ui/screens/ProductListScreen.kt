76c680b68511349e722d3d0f374f2703755f343b
Jefferson <jefferson.ig.ig@gmail.com>
2025-12-05 16:05:11 -0500

    creacion del ProductListScreen.kt

--- ProductManager/app/src/main/java/com/tecsup/productmanager/ui/screens/ProductListScreen.kt
+++ ProductManager/app/src/main/java/com/tecsup/productmanager/ui/screens/ProductListScreen.kt
@@ -1,4 +1,37 @@
 package com.tecsup.productmanager.ui.screens
 
-class ProductListScreen {
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.tecsup.productmanager.data.model.Product
+import com.tecsup.productmanager.ui.components.ProductItem
+import com.tecsup.productmanager.viewmodel.ProductViewModel
+
+@Composable
+fun ProductListScreen(
+    viewModel: ProductViewModel,
+    onCreateClick: () -> Unit,
+    onEditClick: (Product) -> Unit
+) {
+    val list by viewModel.products.collectAsState()
+
+    LazyColumn {
+        items(list) { product ->
+            ProductItem(
+                product = product,
+                onEdit = { onEditClick(product) },
+                onDelete = { viewModel.deleteProduct(product.id) }
+            )
+        }
+    }
+
+    FloatingActionButton(onClick = onCreateClick) {
+        Icon(Icons.Default.Add, contentDescription = "Add")
+    }
 }
