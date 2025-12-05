94c4b8f92c7eefeab8e7586d55782bdaf99821b4
Jefferson <jefferson.ig.ig@gmail.com>
2025-12-05 15:59:21 -0500

    creacion del ProductRepository.kt

--- ProductManager/app/src/main/java/com/tecsup/productmanager/data/repository/ProductRepository.kt
+++ ProductManager/app/src/main/java/com/tecsup/productmanager/data/repository/ProductRepository.kt
@@ -1,4 +1,37 @@
 package com.tecsup.productmanager.data.repository
 
-class ProductRepository {
+import com.google.firebase.firestore.FirebaseFirestore
+import com.tecsup.productmanager.data.model.Product
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class ProductRepository(
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+) {
+
+    private val _products = MutableStateFlow<List<Product>>(emptyList())
+    val products: StateFlow<List<Product>> get() = _products
+
+    fun listenProducts(userId: String) {
+        db.collection("products")
+            .whereEqualTo("userId", userId)
+            .addSnapshotListener { value, error ->
+                if (error != null) return@addSnapshotListener
+                val list = value?.toObjects(Product::class.java) ?: emptyList()
+                _products.value = list
+            }
+    }
+
+    suspend fun addProduct(product: Product) {
+        val id = db.collection("products").document().id
+        db.collection("products").document(id).set(product.copy(id = id))
+    }
+
+    suspend fun updateProduct(product: Product) {
+        db.collection("products").document(product.id).set(product)
+    }
+
+    suspend fun deleteProduct(id: String) {
+        db.collection("products").document(id).delete()
+    }
 }
